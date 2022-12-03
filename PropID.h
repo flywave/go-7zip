15b60b2b638b6c7eade7c0a4f29e51da27bc6c6a
aninggo <aninggo@gmail.com>
2022-12-03 11:13:20 +0800

    u

--- PropID.h
+++ PropID.h
@@ -4,32 +4,32 @@
 #define __7ZIP_PROPID_H
 
 enum PropertyIndexEnum {
-		PROP_INDEX_BEGIN,
+  PROP_INDEX_BEGIN,
 
-		kpidPackSize = PROP_INDEX_BEGIN, //(Packed Size)
-		kpidAttrib, //(Attributes)
-		kpidCTime, //(Created)
-		kpidATime, //(Accessed)
-		kpidMTime, //(Modified)
-		kpidSolid, //(Solid)
-		kpidEncrypted, //(Encrypted)
-		kpidUser, //(User)
-		kpidGroup, //(Group)
-		kpidComment, //(Comment)
-		kpidPhySize, //(Physical Size)
-		kpidHeadersSize, //(Headers Size)
-		kpidChecksum, //(Checksum)
-		kpidCharacts, //(Characteristics)
-		kpidCreatorApp, //(Creator Application)
-		kpidTotalSize, //(Total Size)
-		kpidFreeSpace, //(Free Space)
-		kpidClusterSize, //(Cluster Size)
-		kpidVolumeName, //(Label)
-		kpidPath, //(FullPath)
-		kpidIsDir, //(IsDir)
-		kpidSize, //(Uncompressed Size)
+  kpidPackSize = PROP_INDEX_BEGIN, //(Packed Size)
+  kpidAttrib,                      //(Attributes)
+  kpidCTime,                       //(Created)
+  kpidATime,                       //(Accessed)
+  kpidMTime,                       //(Modified)
+  kpidSolid,                       //(Solid)
+  kpidEncrypted,                   //(Encrypted)
+  kpidUser,                        //(User)
+  kpidGroup,                       //(Group)
+  kpidComment,                     //(Comment)
+  kpidPhySize,                     //(Physical Size)
+  kpidHeadersSize,                 //(Headers Size)
+  kpidChecksum,                    //(Checksum)
+  kpidCharacts,                    //(Characteristics)
+  kpidCreatorApp,                  //(Creator Application)
+  kpidTotalSize,                   //(Total Size)
+  kpidFreeSpace,                   //(Free Space)
+  kpidClusterSize,                 //(Cluster Size)
+  kpidVolumeName,                  //(Label)
+  kpidPath,                        //(FullPath)
+  kpidIsDir,                       //(IsDir)
+  kpidSize,                        //(Uncompressed Size)
 
-		PROP_INDEX_END
+  PROP_INDEX_END
 };
 
 #endif
